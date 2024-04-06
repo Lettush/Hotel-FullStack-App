d4431e4ae7fa9d69cc9b5af66372891dec9f956b
Jared <jaredneilhortaleza11@gmail.com>
2024-04-07 02:05:37 +0800

    removed deprecated code

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const errorHandler = require('./utils/errorHandler');
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
