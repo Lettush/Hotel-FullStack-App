398c768ff5aee2daed6d65097ac67299700e77f3
Jared Neil A. Hortaleza <jaredneilhortaleza11@gmail.com>
2024-06-24 23:14:10 +0800

    added cancel reservation

--- frontend/src/components/Reservations.js
+++ frontend/src/components/Reservations.js
@@ -4,10 +4,37 @@ import { Link, useNavigate } from "react-router-dom";
 const Reservations = () => {
   const [reservations, setReservations] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const token = localStorage.getItem("token");
+
   const navigate = useNavigate();
 
+  const cancelReservation = async (id) => {
+    const token = localStorage.getItem("token");
+    setIsLoading(true);
+
+    await fetch(`http://localhost:4000/api/reservations/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          throw new Error(data.error);
+        } else {
+          navigate(0);
+          console.log(data);
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   useEffect(() => {
+    const token = localStorage.getItem("token");
+
     fetch("http://localhost:4000/api/reservations", {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -36,7 +63,7 @@ const Reservations = () => {
         <div>Loading...</div>
       ) : (
         reservations.map((reservation) => (
-          <div>
+          <div key={reservation._id}>
             <ul>
               <Link to={`/rooms/${reservation.roomId}`}>Room</Link>
               <li>{reservation.checkInDate}</li>
@@ -44,7 +71,15 @@ const Reservations = () => {
               <li>${reservation.totalAmount}</li>
               <li>{reservation.paymentIntentId !== "" ? "Paid" : "Unpaid"}</li>
               <li>{reservation.user}</li>
-              {reservation.canceled && <li>Canceled</li>}
+              <li>
+                {reservation.canceled ? (
+                  <span>Canceled</span>
+                ) : (
+                  <button onClick={() => cancelReservation(reservation._id)}>
+                    Cancel
+                  </button>
+                )}
+              </li>
             </ul>
           </div>
         ))
